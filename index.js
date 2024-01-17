940ee0f175e50d00549bc3e6f4ff88b12d80537b
Igor Rudolf <azazil47@gmail.com>
2024-01-17 11:03:19 +0700

    Реализовано подключение к БД

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+require('dotenv').config()
 const express = require("express")
-const dotenv = require('dotenv').config()
+const sequelize = require('./db')
+
 
 const app = express()
 
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await  sequelize.sync()
+        app.listen(3000, HOST,() => {
+            console.log(`Server is started is ${HOST} on port: ${PORT}` )
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 const HOST = process.env.HOST || localhost
 const PORT = process.env.PORT || 7000
-app.listen(3000, HOST,() => {
-    console.log(`Server is started is ${HOST} on port: ${PORT}` )
-})
+
+start()
