7a4d805d42eb6f33584a7aa9e0e09f7eb0a37116
Igor Rudolf <azazil47@gmail.com>
2024-01-22 09:57:37 +0700

    Сделана схема данных v1

--- models/models.js
+++ models/models.js
@@ -4,69 +4,121 @@ const { DataTypes} = require("sequelize")
 // Товары
 const Product = sequelize.define('product', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
+    name: {type: DataTypes.STRING, unique: true, notNull: true},
+},
+{
+    timestamps: false,
 })
 
 // Категории товаров
 const Category = sequelize.define('category', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
+    name: {type: DataTypes.STRING, unique: true, notNull: true},
+},
+{
+    timestamps: false,
 })
 
 // Бранды
 const Brand = sequelize.define('brand', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
+    name: {type: DataTypes.STRING, unique: true, notNull: true},
+},
+{
+    timestamps: false,
 })
-
 
 // Картинки
 const Image = sequelize.define('image', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
+    name: {type: DataTypes.STRING, unique: true, notNull: true},
+},
+{
+    timestamps: false,
 })
 
 // Свойства
 const Property = sequelize.define('property', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
+    name: {type: DataTypes.STRING, unique: true, notNull: true},
+
+},
+{
+    timestamps: false,
 })
 
 // Значение
 const Value = sequelize.define('value', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, notNull: true}
+    name: {type: DataTypes.STRING, notNull: true},
+},
+{
+    timestamps: false,
+})
+
+const ProductProperty = sequelize.define('ProductProperty', {
+},
+{
+    timestamps: false,
+})
+//
+const PropertyValue = sequelize.define('PropertyValue', {
+},
+{
+    timestamps: false,
+})
+//
+const PropertyCategory = sequelize.define('PropertyCategory', {
+
+},
+{
+    timestamps: false,
 })
 
 
 // Один бренд имеет одно изображение (логотип)
-Brand.hasOne(Image)
-Image.belongsTo(Brand)
+Image.hasOne(Brand)
+Brand.belongsTo(Image)
 
-// Одиному бренду может соответствовать несколько товаров
+// // Одному бренду может соответствовать несколько товаров
 Brand.hasMany(Product)
 Product.belongsTo(Brand)
-
-// В категорию может входить несколько товаров
+//
+// // В категорию может входить несколько товаров
 Category.hasMany(Product)
 Product.belongsTo(Category)
-
-// У одного товара может быть множество рисунков
-Image.hasMany(Product)
-Product.belongsTo(Image)
-
+//
+// // У одного товара может быть множество рисунков
+Product.hasMany(Image)
+Image.belongsTo(Product)
+//
 // У одной категории может быть один рисунок
-Category.hasOne(Image)
-Image.belongsTo(Category)
-
+Image.hasOne(Category)
+Category.belongsTo(Image)
+//
 //Свойство может быть у нескольких категорий и несколько категорий иметь одно свойство
-Property.belongsToMany(Category, {through:'PropertyCategory'})
-Category.belongsToMany(Property, {through:'PropertyCategory'})
-
+Property.belongsToMany(Category, {through:PropertyCategory})
+Category.belongsToMany(Property, {through:PropertyCategory})
+//
 //Одна категория может иметь несколько свойств, так и одно свойство может соответствовать нескольким категориям
-Property.belongsToMany(Value, {through:'PropertyValue'})
-Value.belongsToMany(Property, {through:'PropertyValue'})
-
+Property.belongsToMany(Value, {through:PropertyValue})
+Value.belongsToMany(Property, {through:PropertyValue})
+//
 //У одного продукта может быть несколько свойств, а также одно свойство может принадлежать нескольким продуктам
-Product.belongsToMany(Property, {through:'ProductProperty'})
-Property.belongsToMany(Product, {through:'ProductProperty'})
+Product.belongsToMany(Property, {through:ProductProperty})
+Property.belongsToMany(Product, {through:ProductProperty})
+//
+Value.hasMany(ProductProperty)
+ProductProperty.belongsTo(Value)
+
+module.exports = {
+    Product,
+    Category,
+    Brand,
+    Image,
+    Property,
+    Value,
+    ProductProperty,
+    PropertyValue,
+    PropertyCategory
+}
--- models/product.js
+++
@@ -1,7 +0,0 @@
-import sequelize from "../db";
-import {DataTypes} from "sequelize";
-
-export const Product = sequelize.define('product', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, notNull: true}
-})
